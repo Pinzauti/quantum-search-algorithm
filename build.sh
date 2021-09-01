104f264b6e8fab31dd34d57802b78a14cb08610e
Francesco Pinzauti <francescopinzauti@gmail.com>
2021-09-01 19:11:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ if [ ! -e "$INSTALL_TL_SUCCESS" ]; then
   curl -L "$INSTALL_TL_URL" | tar xz --one-top-level=itl --strip-components=1
   echo "$TEXLIVE_PROFILE" > texlive.profile
   itl/install-tl --profile=texlive.profile
-  "$TEXLIVE_BIN/tlmgr" install latexmk texliveonfly biblatex braket frontespizio mathtools classicthesis
+  "$TEXLIVE_BIN/tlmgr" install latexmk texliveonfly biblatex braket amsmath amssymb amsthm mathtools frontespizio classicthesis graphicx tikz, circuitikz
   echo "[$0] Installed TeX Live."
 
   touch "$INSTALL_TL_SUCCESS"
